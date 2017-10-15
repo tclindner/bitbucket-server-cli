86a034fa2f3dc4765104a11df39a07c95e193ae1
tclindner <tclindner@users.noreply.github.com>
2017-10-14 23:00:11 -0500

    Refactor API and plugin for better output and organization

---
+++ src/api/RequestHelper.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/* eslint id-length: 'off', class-methods-use-this: 'off', no-magic-numbers: 'off', no-param-reassign: 'off', no-negated-condition: 'off' */
+const request = require('request');
+const OK = 200;
+
+class RequestHelper {
+
+  /**
+   * Recursive API walker that traverses Bitbucket's paged API
+   *
+   * @static
+   * @param {String} url API endpoint to call
+   * @param {Object} options Configuration for request
+   *        {String} options.queryParams Query string parameters to add to the request
+   *        {Number} options.start Page to start on
+   *        {Number} options.limit Number of items to limit in the request
+   * @returns {Promise} A promise that will resolve to the value of the API call
+   * @memberof RequestHelper
+   */
+  static walker(url, options) {
+    const start = options.hasOwnProperty('start') ? options.start : 0;
+    const limit = options.hasOwnProperty('limit') ? options.limit : 25;
+    const queryParams = options.hasOwnProperty('queryParams') ? options.queryParams : '';
+    const requestUrl = `${url}?start=${start}&limit=${limit}${queryParams}`;
+
+    return new Promise((resolve, reject) => {
+      request.get(requestUrl, options.auth, (error, response, body) => {
+        if (!error && response.statusCode === OK) {
+          const data = JSON.parse(body);
+          let values = data.values;
+
+          if (data.isLastPage) {
+            resolve(values);
+          } else {
+            const walkerOptions = {
+              start: data.nextPageStart,
+              limit: data.limit,
+              queryParams
+            };
+
+            this.walker(url, walkerOptions).then((additionalValues) => {
+              values = values.concat(additionalValues);
+
+              resolve(values);
+            }).catch(function(err) {
+              reject(err);
+            });
+          }
+        } else {
+          reject(new Error(error));
+        }
+      });
+    });
+  }
+
+  /**
+   * Non-paged API call
+   *
+   * @static
+   * @param {String} url API endpoint to call
+   * @returns {Promise} A promise that will resolve to the value of the API call
+   * @memberof RequestHelper
+   */
+  static nonPagedRequest(auth, url) {
+    return new Promise((resolve, reject) => {
+      request.get(url, auth, (error, response, body) => {
+        if (!error && response.statusCode === OK) {
+          resolve(JSON.parse(body));
+        } else {
+          reject(new Error(error));
+        }
+      });
+    });
+  }
+
+}
+
+module.exports = RequestHelper;
