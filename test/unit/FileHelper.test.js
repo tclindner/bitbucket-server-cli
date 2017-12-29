56bc6caeb0af5fd8f0e34e162c41a431481a731a
tclindner <tclindner@users.noreply.github.com>
2017-12-28 21:58:35 -0600

    Add tests for reading in files and bitbucket api client

---
+++ test/unit/FileHelper.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+const chai = require('chai');
+const sinon = require('sinon');
+const FileHelper = require('./../../src/FileHelper');
+
+const should = chai.should();
+
+describe('FileHelper Unit Tests', function() {
+  beforeEach(function() {
+    const stub = sinon.stub(fs, 'readFileSync');
+
+    stub.returns('{"key": "value"}');
+  });
+
+  afterEach(function() {
+    fs.readFileSync.restore();
+  });
+
+  it('JSON file is read', function() {
+    const filePath = './config.json';
+    const result = FileHelper.readJsonFile(filePath);
+    const expected = {
+      'key': 'value'
+    };
+
+    result.should.deep.equal(expected);
+  });
+});
