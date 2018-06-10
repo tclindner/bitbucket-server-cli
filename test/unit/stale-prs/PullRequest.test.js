9bc39f32b063e3ffdf6e8d5e36b7bda1a5f40c39
tclindner <tclindner@users.noreply.github.com>
2018-06-09 20:54:31 -0500

    Add basic tests for stale pr

---
+++ test/unit/stale-prs/PullRequest.test.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const chai = require('chai');
+const PullRequest = require('./../../../src/plugins/stale-prs/PullRequest');
+
+const should = chai.should();
+
+describe('PullRequest Unit Tests', function() {
+  describe('constructor', function() {
+    context('when all arguments are passed', function() {
+      let pullRequest;
+      const dummyPullRequestObj = {
+          id: 1,
+          title: 'title',
+          author: {
+              user: {
+                  displayName: 'Some, Dude'
+              }
+          },
+          createdDate: 1457139523140,
+          updatedDate: 1457139523140,
+          fromRef: {
+              displayId: 1
+          },
+          toRef: {
+              displayId: 2
+          }
+      };
+
+      before(function() {
+        pullRequest = new PullRequest('PROJECT', 'repo', dummyPullRequestObj);
+      });
+
+      it('_project should be equal to PROJECT', function() {
+        pullRequest._project.should.equal('PROJECT');
+      });
+
+      it('_repo should be equal to repo', function() {
+        pullRequest._repo.should.equal('repo');
+      });
+
+      it('_id should be 1', function() {
+        pullRequest._id.should.equal(1);
+      });
+
+      it('_title should be equal to title', function() {
+        pullRequest._title.should.equal('title');
+      });
+
+      it('_author should be equal to Some, Dude', function() {
+        pullRequest._author.should.equal('Some, Dude');
+      });
+
+      it('_createdDate should be equal to 1457139523140', function() {
+        pullRequest._createdDate.should.equal('2016-03-04');
+      });
+
+      it('_fromBranchName should be equal to 1', function() {
+        pullRequest._fromBranchName.should.equal(1);
+      });
+
+      it('_toBranchName should be equal to 2', function() {
+        pullRequest._toBranchName.should.equal(2);
+      });
+    });
+  });
+});
---
+++ test/unit/stale-prs/Reporter.test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const chai = require('chai');
+const chalk = require('chalk');
+const sinon = require('sinon');
+const PullRequest = require('./../../../src/plugins/stale-prs/PullRequest');
+const Reporter = require('./../../../src/plugins/stale-prs/Reporter');
+
+const should = chai.should();
+
+describe('Reporter Unit Tests', function() {
+  describe('write method', function() {
+    context('when an array is passed', function() {
+      let dummyPullRequestObj = {
+        id: 1,
+        title: '',
+        author: {
+          user: {
+            displayName: 'Some, Dude'
+          }
+        },
+        createdDate: 1457139523140,
+        updatedDate: 1457139523140,
+        fromRef: {
+          displayId: 1
+        },
+        toRef: {
+          displayId: 1
+        }
+      };
+      let spy;
+
+      beforeEach(function() {
+        spy = sinon.spy(console, 'log');
+      });
+
+      afterEach(function() {
+        console.log.restore();
+      });
+
+      it('with an array with zero pull requests is passed a formatted message should be returned saying there are no pull requests', function() {
+        const pullRequests = [];
+        const output = chalk.green.bold('No stale pull requests found!');
+
+        Reporter.write(pullRequests);
+        spy.withArgs(output).calledOnce.should.be.true;
+      });
+
+      it('when an array with one pull request is passed a formatted message should be returned saying there is one pull request', function() {
+        const pullRequests = [];
+        const output = `${chalk.red.bold(1)} stale pull request found.`;
+
+        pullRequests.push(new PullRequest('PROJECT', 'repo', dummyPullRequestObj));
+
+        Reporter.write(pullRequests);
+        spy.calledTwice.should.be.true;
+        spy.secondCall.calledWithExactly(output).should.be.true;
+      });
+
+      it('when an array with twos pull requests are passed a formatted message should be returned saying there are two pull requests', function() {
+        const pullRequests = [];
+        const output = `${chalk.red.bold(2)} stale pull requests found.`;
+
+        pullRequests.push(new PullRequest('PROJECT', 'repo', dummyPullRequestObj));
+        pullRequests.push(new PullRequest('PROJECT', 'repo', dummyPullRequestObj));
+
+        Reporter.write(pullRequests);
+        spy.calledThrice.should.be.true;
+        spy.thirdCall.calledWithExactly(output).should.be.true;
+      });
+    });
+  });
+});
